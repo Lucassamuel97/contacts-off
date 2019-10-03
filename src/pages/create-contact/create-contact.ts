d9af420ad9491200db07add57d8fbb8763993f8f
Lucas samuel <lukassamuka88@gmail.com>
2019-10-03 19:18:12 -0300

    Formulario cadastro do contato

--- src/pages/create-contact/create-contact.ts
+++ src/pages/create-contact/create-contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ContactsProvider } from '../../providers/contacts/contacts';
 
 /**
  * Generated class for the CreateContactPage page.
@@ -14,12 +15,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'create-contact.html',
 })
 export class CreateContactPage {
+  model: Contact;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.model = new Contact();
+    this.model.name = 'Novo contato';
+    this.model.gender = 'male';
   }
 
+  createContact(){
+  }
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateContactPage');
   }
 
+}
+
+export class Contact {
+  name: string;
+  gender: string;
 }
